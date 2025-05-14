d4e700e3787b33dc6290ee3e1d08f0207b4ac323
curiousandcuriouser <204502050+curiousandcuriouser@users.noreply.github.com>
2025-05-14 22:25:20 +0200

    Add cart.push upon button click

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -23,7 +23,34 @@ products.forEach((product) => {
           <div class="product-price">
             $${(product.priceCents / 100).toFixed(2)}
           </div>
+
+           <div class="product-quantity-container">
+            <select>
+              <option selected value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
           </div>
+
+          <div class="product-spacer"></div>
+
+          <div class="added-to-cart">
+            <img src="images/icons/checkmark.png">
+            Added
+          </div>
+
+          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-name="${product.name}">
+            Add to Cart
+          </button>
+
+        </div>
   `;
 
 })
@@ -33,3 +60,29 @@ console.log(productsHTML);
 document.querySelector('.js-products-grid')
   .innerHTML = productsHTML;
 
+document.querySelectorAll('.js-add-to-cart')
+  .forEach((button) => {
+    button.addEventListener('click', () => {
+     const productName = button.dataset.productName;
+
+     let matchingItem;
+     
+     cart.forEach((item) => {
+      if (productName === item.productName) {
+        matchingItem = item;
+      }
+     })
+
+     if (matchingItem) {
+      matchingItem.quantity += 1;
+     } else {
+       cart.push({
+        productName: productName,
+        quantity: 1
+     })
+    }
+
+     console.log(cart);
+    })});
+
+    //12:14:47
