0134544bbc31b3ca435306b11cfe49f99ac0c970
curiousandcuriouser <204502050+curiousandcuriouser@users.noreply.github.com>
2025-05-16 21:18:11 +0200

    Save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,20 +1,27 @@
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if (!cart) {
+  cart = [{
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity: 2
   }, {
     productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
     quantity: 1
-  }, {
-    productId: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
-    quantity: 1
-}];
+  }];
+}
+
+function saveToStorage () {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+
 
 export function addToCart (productId) {
   let matchingItem;
      
      cart.forEach((cartItem) => {
       if (productId === cartItem.productId) {
-        matchingItem = carIttem;
+        matchingItem = cartItem;
       }
      })
 
@@ -26,6 +33,8 @@ export function addToCart (productId) {
         quantity: 1
      })
     }
+
+    saveToStorage();
 };
 
 export function removeFromCart(productId) {
@@ -34,8 +43,10 @@ export function removeFromCart(productId) {
     if (cartItem.productId !== productId) {
       newCart.push(cartItem);
     }
-    
+
   })
 
   cart = newCart;
+
+  saveToStorage();
 };
